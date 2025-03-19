4373bda63a932ab479e2ebe35b6478b432b3e7b5
saeed abdulrauf <abdulraufsaeed@gmail.com>
2025-03-19 14:43:16 -0700

    worked on the homepage

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+    import Btn from "../components/ui/Btn.vue";
+</script>
 
 <template>
-    <div class="">
+    <div class="m-0 p-0">
         <div class="shadow-md fixed top-0 w-full bg-white z-10">
             <div
                 class="flex justify-between items-center text-sm p-4 md:text-lg md:px-6 lg:px-10 max-w-[1440px] mx-auto"
@@ -65,80 +67,237 @@
             </div>
         </div>
         <!--MedicationSection-->
-        <div>
-            <section class="py-10 px-5">
-                <h2 class="text-2xl font-semibold text-gray-800 text-center">
-                    Featured Products
-                </h2>
-                <div
-                    class="mt-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6"
-                >
-                    <div class="p-4 border rounded-lg shadow-md text-center">
-                        <img
-                            src="https://via.placeholder.com/150"
-                            alt="Medicine"
-                            class="mx-auto w-24 h-24"
-                        />
-                        <h3 class="mt-3 font-medium text-gray-800">
-                            Pain Reliever
-                        </h3>
-                        <p class="text-gray-600">$5.99</p>
-                        <button
-                            class="mt-3 px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700 transition"
+        <div class="bg-white">
+            <h1
+                class="py-3 text-lg font-medium text-orange-600 text-center md:font-semibold"
+            >
+                Featured Product
+            </h1>
+            <div
+                class="px-4 mb-5 mx-auto grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-4 md:px-6 md:gap-5 md:mb-8 max-w-[1440px]"
+            >
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/drug1.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
                         >
-                            Order Now
-                        </button>
+                            <h1 class="text-center text-white p-1">
+                                Pain Relief
+                            </h1>
+                        </div>
                     </div>
-                    <div class="p-4 border rounded-lg shadow-md text-center">
-                        <img
-                            src="https://via.placeholder.com/150"
-                            alt="Medicine"
-                            class="mx-auto w-24 h-24"
-                        />
-                        <h3 class="mt-3 font-medium text-gray-800">
-                            Cough Syrup
-                        </h3>
-                        <p class="text-gray-600">$8.50</p>
-                        <button
-                            class="mt-3 px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700 transition"
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/drug2.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
                         >
-                            Order Now
-                        </button>
+                            <h1 class="text-center text-white p-1">
+                                Cold & Flu
+                            </h1>
+                        </div>
                     </div>
-                    <div class="p-4 border rounded-lg shadow-md text-center">
-                        <img
-                            src="https://via.placeholder.com/150"
-                            alt="Medicine"
-                            class="mx-auto w-24 h-24"
-                        />
-                        <h3 class="mt-3 font-medium text-gray-800">
-                            Vitamin C Tablets
-                        </h3>
-                        <p class="text-gray-600">$12.00</p>
-                        <button
-                            class="mt-3 px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700 transition"
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/drug3.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
                         >
-                            Order Now
-                        </button>
+                            <h1 class="text-center text-white p-1">
+                                Supplements
+                            </h1>
+                        </div>
                     </div>
-                    <div class="p-4 border rounded-lg shadow-md text-center">
-                        <img
-                            src="../../public/favicon.ico"
-                            alt="Medicine"
-                            class="mx-auto w-24 h-24"
-                        />
-                        <h3 class="mt-3 font-medium text-gray-800">
-                            Antibiotics
-                        </h3>
-                        <p class="text-gray-600">$15.00</p>
-                        <button
-                            class="mt-3 px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700 transition"
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/drug4.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
                         >
-                            Order Now
-                        </button>
+                            <h1 class="text-center text-white p-1">
+                                Antibiotics
+                            </h1>
+                        </div>
+                    </div>
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/syrup1.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
+                        >
+                            <h1 class="text-center text-white p-1">
+                                Digestive Health
+                            </h1>
+                        </div>
+                    </div>
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/vitamin.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
+                        >
+                            <h1 class="text-center text-white p-1">Vitamins</h1>
+                        </div>
+                    </div>
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/drug5.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
+                        >
+                            <h1 class="text-center text-white p-1">Syrup</h1>
+                        </div>
+                    </div>
+                </router-link>
+                <router-link to="/signup"
+                    ><div
+                        class="bg-gray-500 bg-[url('/public/skincare.jpg')] bg-no-repeat bg-cover p-1 rounded-xl"
+                    >
+                        <div
+                            class="bg-black bg-opacity-60 rounded-b-xl mt-40 md:mt-48"
+                        >
+                            <h1 class="text-center text-white p-1">
+                                Skin Care
+                            </h1>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+        <!--DeliverySection-->
+        <div class="bg-gray-50 py-3 px-4 text-center md:py-5 md:px-6">
+            <h2 class="text-lg font-semibold text-orange-600 my-2">
+                Fast & Reliable Delivery
+            </h2>
+            <p class="text-gray-600">
+                Get your medicines delivered to your doorstep in no time
+            </p>
+            <button
+                class="mt-4 px-4 py-2 bg-orange-600 text-white rounded-md font-semibold hover:bg-orange-700 transition"
+            >
+                Order Now
+            </button>
+        </div>
+        <!--FooterSection-->
+        <div class="bg-gray-800 px-4 py-5 grid place-items-center">
+            <div
+                class="grid grid-cols-1 gap-9 lg:w-full lg:grid-cols-3 md:px-6 max-w-[1440px]"
+            >
+                <div class="">
+                    <h2
+                        class="text-sm font-semibold text-blue-500 py-3 md:py-5"
+                    >
+                        Let's Connect
+                    </h2>
+                    <p
+                        class="text-gray-400 text-sm flex items-center gap-3 pb-2"
+                    >
+                        <i class="pi pi-ethereum block text-white"></i>
+                        Market junction, Kumasi, Ghana
+                    </p>
+                    <p
+                        class="text-gray-400 text-sm flex items-center gap-3 pb-2"
+                    >
+                        <i class="pi pi-phone block text-white"></i> +233 XXX
+                        XXX XXX
+                    </p>
+                    <p
+                        class="text-gray-400 text-sm flex items-center gap-3 pb-2"
+                    >
+                        <i class="pi pi-whatsapp block text-white"></i> +233 XXX
+                        XXX XXX
+                    </p>
+                    <p class="text-gray-400 text-sm flex items-center gap-3">
+                        <i class="pi pi-envelope block text-white"></i>
+                        support@vanellepharmacy.com
+                    </p>
+                </div>
+                <div>
+                    <h2
+                        class="font-semibold text-sm text-blue-500 py-3 md:py-5"
+                    >
+                        Social Media Links
+                    </h2>
+                    <div class="flex gap-4 w-max">
+                        <a href="#" class="text-gray-400 hover:text-white">
+                            <i class="pi pi-facebook text-xl"></i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-white">
+                            <i class="pi pi-instagram text-xl"></i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-white">
+                            <i class="pi pi-twitter text-xl"></i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-white">
+                            <i class="pi pi-tiktok text-xl"></i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-white">
+                            <i class="pi pi-linkedin text-xl"></i>
+                        </a>
+                        <a href="#" class="text-gray-400 hover:text-white">
+                            <i class="pi pi-google text-xl"></i>
+                        </a>
                     </div>
                 </div>
-            </section>
+                <div class="">
+                    <h2
+                        class="text-sm font-semibold text-blue-500 py-3 md:py-5"
+                    >
+                        Contact Us
+                    </h2>
+                    <form
+                        class="md:w-[300px] lg:w-auto max-w-[450px] space-y-3 text-center text-sm"
+                    >
+                        <input
+                            type="text"
+                            placeholder="Your Name"
+                            class="w-full p-2 rounded-md bg-transparent border border-gray-700 text-white placeholder-gray-400 focus:outline-none focus:border-white"
+                        />
+                        <input
+                            type="email"
+                            placeholder="Your Email"
+                            class="w-full p-2 rounded-md bg-transparent border border-gray-700 text-white placeholder-gray-400 focus:outline-none focus:border-white"
+                        />
+                        <textarea
+                            rows="3"
+                            placeholder="Your Message"
+                            class="w-full p-2 rounded-md bg-transparent border border-gray-700 text-white placeholder-gray-400 focus:outline-none focus:border-white"
+                        ></textarea>
+                        <Btn btnName="Send Messsage" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!--CopyRightSection-->
+
+        <div
+            class="border-gray-700 py-4 text-center text-gray-400 text-sm bg-gray-800"
+        >
+            <p>&copy; Vanelle Pharmacy. All Rights Reserved.</p>
+        </div>
+        <div class="bg-gray-800 px-4 md:px-6 lg:px-10 pt-5">
+            <div
+                class="max-w-[1440px] border-t border-gray-400 pt-5 md:mx-auto md:w-[300px] lg:w-auto"
+            >
+                <p class="text-white text-lg font-semibold flex gap-3 pb-4">
+                    <i class="pi pi-slack text-blue-600 text-2xl"></i>venella
+                    pharmacy
+                </p>
+            </div>
         </div>
     </div>
 </template>
