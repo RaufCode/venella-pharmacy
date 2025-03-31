022a7c96ab741efb449c62b801cc71ba1da73b43
Manaf <manaf18.m@gmail.com>
2025-03-31 20:24:10 +0000

    fix: configure server to handle cors requests. *Error.txt deleted

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const authRoutes = require('./routes/auth')
-const mainRoutes = require('./routes/main')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const mainRoutes = require('./routes/main');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(bodyParser.json());
 
