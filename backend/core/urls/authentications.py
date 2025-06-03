53997f26ff15ba490d75152206fa5339bcc0cace
Manaf <manaf18.m@gmail.com>
2025-06-03 09:13:01 +0000

    add change password functionality with the endpoints

--- backend/core/urls/authentications.py
+++ backend/core/urls/authentications.py
@@ -1,15 +1,35 @@
 from django.urls import path
-from core.views.authentications import SignInView
+from core.views.authentications import SignInView, PasswordViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 
 AUTH_URLS = [
     path("auth/login/", SignInView.as_view()),
-    path("auth/token/", TokenObtainPairView.as_view()),
-    path("auth/token/refresh/", TokenRefreshView.as_view()),
-    path("auth/token/verify/", TokenVerifyView.as_view()),
+    path(
+        "auth/token/",
+        extend_schema_view(
+            post=extend_schema(summary="Obtain Tokens", tags=["Tokens"])
+        )(TokenObtainPairView.as_view()),
+    ),
+    path(
+        "auth/token/refresh/",
+        extend_schema_view(
+            post=extend_schema(summary="Refresh Tokens", tags=["Tokens"])
+        )(TokenRefreshView.as_view()),
+    ),
+    path(
+        "auth/token/verify/",
+        extend_schema_view(
+            post=extend_schema(summary="Verify Tokens", tags=["Tokens"])
+        )(TokenVerifyView.as_view()),
+    ),
+    path(
+        "auth/password/change/<str:user_id>/",
+        PasswordViewSet.as_view({"post": "change_password"}),
+    ),
 ]
--- backend/core/views/authentications.py
+++ backend/core/views/authentications.py
@@ -1,122 +1,44 @@
 from core.views._base import *
 from core.selectors.accounts import *
+from core.utils.general import validate_posted_data
 
 
-# class PasswordViewSet(viewsets.ViewSet):
-#     """Manages password change and reset"""
+class PasswordViewSet(viewsets.ViewSet):
+    """Manages password change and reset"""
 
-#     def request_token(self, request):
-#         """Request password reset token"""
-#         data = request.data
-#         user = get_user_by_email(data.get("email"))
-#         if not user:
-#             context = {
-#                 "detail": "No account found",
-#                 "errors": {"email": [f"no account exists for '{data.get('email')}' "]},
-#             }
-#             raise NotFound(context)
+    @change_password_schema
+    def change_password(self, request, user_id):
+        """Change password for a user account"""
 
-#         email_thread = threading.Thread(
-#             target=password_reset, args=[user, user.email, OTP_LENGTH]
-#         )
-#         email_thread.start()
+        err, errors = validate_posted_data(
+            request.data, ["current_password", "new_password"]
+        )
+        if err:
+            context = {"detail": "missing required data", "errors": errors}
+            raise ValidationError(context)
 
-#         context = {"detail": "password reset verification token sent"}
-#         return Response(context, status.HTTP_200_OK)
+        current_password = request.data.get("current_password")
+        new_password = request.data.get("new_password")
 
-#     def reset_password(self, request):
-#         """
-#         Reset password for a user account
-#         """
-#         email = request.data.get("email")
-#         otp = request.data.get("otp")
-#         password = request.data.get("password")
+        user = get_user_account_by_id(user_id)
+        if not user:
+            context = {
+                "detail": "user not found",
+                "errors": {
+                    "email": [f"invalid pk'{user_id}' - object does not exist "]
+                },
+            }
+            raise NotFound(context)
 
-#         # validate the provided data
-#         err: bool = False
-#         errors: dict = {}
-#         if not email:
-#             err = True
-#             errors.update({"email": ["this field is required"]})
-#         if not password:
-#             err = True
-#             errors.update({"password": ["this field is required"]})
-#         if not otp:
-#             err = True
-#             errors.update({"otp": ["this field is required"]})
+        if not user.check_password(current_password):
+            context = {"detail": "incorrect current password"}
+            raise NotAcceptable(context)
 
-#         if err:
-#             context = {"detail": "invalid data", "errors": errors}
-#             raise ValidationException(context)
+        user.set_password(new_password)
+        user.save()
 
-#         user = get_user_by_email(email)
-#         if not user:
-#             context = {
-#                 "detail": "user not found",
-#                 "errors": {"email": [f"no account found for '{email}' "]},
-#             }
-#             raise NotFound(context)
-
-#         token = get_password_reset_token(email)
-#         if not token:
-#             context = {"detail": "request verification token"}
-#             raise NotFound(context)
-
-#         if otp != token.otp:
-#             context = {"detail": "invalid otp provided"}
-#             raise NotAcceptable(context)
-
-#         if UTC.localize(datetime.now()) >= token.time_generated + timedelta(minutes=30):
-#             context = {"detail": "expired otp, request a new one"}
-#             raise NotAcceptable(context)
-
-#         user.set_password(password)
-#         user.save()
-#         token.delete()
-
-#         e_t = threading.Thread(target=password_reset_confirmation, args=[user, email])
-#         e_t.start()
-
-#         context = {"detail": "password reset successful"}
-#         return Response(context, status=status.HTTP_200_OK)
-
-#     def change_password(self, request, user_id):
-#         """Change password for a user account"""
-#         current_password = request.data.get("current_password")
-#         password = request.data.get("password")
-
-#         err: bool = False
-#         errors: dict = {}
-#         if not current_password:
-#             err = True
-#             errors.update({"current_password": ["this field is required"]})
-#         if not password:
-#             err = True
-#             errors.update({"password": ["this field is required"]})
-
-#         if err:
-#             context = {"detail": "missing required data", "errors": errors}
-#             raise ValidationException(context)
-
-#         user = get_user_by_id(user_id)
-#         if not user:
-#             context = {
-#                 "detail": "user not found",
-#                 "errors": {
-#                     "email": [f"invalid pk'{user_id}' - object does not exist "]
-#                 },
-#             }
-#             raise NotFound(context)
-
-#         if not user.check_password(current_password):
-#             context = {"detail": "incorrect current password"}
-#             raise NotAcceptable(context)
-
-#         user.set_password(password)
-#         user.save()
-
-#         context = {"detail": "password successfully changed"}
-#         return Response(context, status.HTTP_200_OK)
+        context = {"detail": "password successfully changed"}
+        return Response(context, status.HTTP_200_OK)
 
 
 class SignInView(APIView):
