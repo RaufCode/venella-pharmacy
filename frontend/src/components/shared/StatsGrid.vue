1fb77a6d9bd05a55f6a437160aa4e1cf0073a565
saeed abdulrauf <abdulraufsaeed@gmail.com>
2025-06-17 13:35:05 -0700

    removed the bg-color

--- frontend/src/components/shared/StatsGrid.vue
+++ frontend/src/components/shared/StatsGrid.vue
@@ -8,7 +8,7 @@
 </script>
 
 <template>
-    <div class="min-h-screen bg-gray-50">
+    <div class="min-h-full">
         <!-- Stats Grid -->
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
             <div
